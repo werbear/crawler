12aeba0d1198b1d8df966b5592ef3d61c061aa30
werbear <st.manipa@gmail.com>
2022-10-02 19:21:04 +0200

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ import cssutils as cs
 def argsParser():
     pars = argparse.ArgumentParser()
     pars.add_argument("--url","-u", help="url to crawl")
-    pars.add_argument("--output","-o", help="Path to output")
+    pars.add_argument("--list","-l", help="list of urls to crawl")
     return pars
 
 def get_url_content(url):
@@ -20,11 +20,19 @@ def get_url_content(url):
         ws = cs.parseStyle(elem.find("a").get("style"))
         print(f"Link: {elem.find('a').get('href')}")
 
+def ifFile(urllist):
+    with open(urllist,"r") as f:
+        for elem in f:
+            get_url_content(elem)
+        
 def main():
     pars = argsParser()
     args = pars.parse_args()
-
-    get_url_content(args.url)
+    
+    if args.url == None and args.list:
+        ifFile(args.list)
+    else:
+        get_url_content(args.url)
 
 if __name__ == "__main__":
     main()
